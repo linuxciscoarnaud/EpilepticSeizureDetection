df0fec10f6bcf25048a9055316e5c7dce901e980
Kenfack Arnaud <arnaudkenfack@yahoo.fr>
2019-09-19 12:55:18 +0100

    Update NetworkConfig.java

--- src/main/java/com/epilepticseizuredetection/NetworkConfig.java
+++ src/main/java/com/epilepticseizuredetection/NetworkConfig.java
@@ -28,55 +28,56 @@ public class NetworkConfig {
 		
 		// LeNet config
 		MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-				.seed(params.getSeed())
-				.optimizationAlgo(params.getOptimizationAlgorithm())
-				.activation(params.getActivation())
-                .weightInit(params.getWeightInit())
-                .updater(params.getUpdater())
-                .cacheMode(params.getCacheMode())
-                .trainingWorkspaceMode(params.getWorkspaceMode())
-                .inferenceWorkspaceMode(params.getWorkspaceMode())
-                .cudnnAlgoMode(params.getCudnnAlgoMode())
-                .convolutionMode(ConvolutionMode.Same)
-				.list()
+			.seed(params.getSeed())
+			.optimizationAlgo(params.getOptimizationAlgorithm())
+			.activation(params.getActivation())
+                        .weightInit(params.getWeightInit())
+                        .updater(params.getUpdater())
+                        .cacheMode(params.getCacheMode())
+                        .trainingWorkspaceMode(params.getWorkspaceMode())
+                        .inferenceWorkspaceMode(params.getWorkspaceMode())
+                        .cudnnAlgoMode(params.getCudnnAlgoMode())
+                        .convolutionMode(ConvolutionMode.Same)
+                        .l2(0.005)
+			.list()
 				
-				// block 1
-				.layer(0, new ConvolutionLayer.Builder(new int[] {5, 5}, new int[] {1, 1})
-						.name("cnn1")
-						.activation(Activation.RELU)
+			// block 1
+			.layer(0, new ConvolutionLayer.Builder(new int[] {5, 5}, new int[] {1, 1})
+				.name("cnn1")
+				.activation(Activation.RELU)
                         .nIn(params.getChannels())
                         .nOut(20)                        
                         .build())
-				.layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, new int[] {2, 2}, new int[] {2, 2})
-						.name("maxpool1")
-						.build())
+			.layer(1, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, new int[] {2, 2}, new int[] {2, 2})
+				.name("maxpool1")
+				.build())
 				
-				// block 2
-				.layer(2, new ConvolutionLayer.Builder(new int[] {5, 5}, new int[] {1, 1})
-						.name("cnn2")
-						.activation(Activation.RELU)
-                        .nOut(50)                        
-                        .build())
-				.layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, new int[] {2, 2}, new int[] {2, 2})
-						.name("maxpool2")
-						.build())
+			// block 2
+			.layer(2, new ConvolutionLayer.Builder(new int[] {5, 5}, new int[] {1, 1})
+				.name("cnn2")
+				.activation(Activation.RELU)
+                                .nOut(50)                        
+                                .build())
+		        .layer(3, new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX, new int[] {2, 2}, new int[] {2, 2})
+				.name("maxpool2")
+				.build())
 				
-				// fully connected
-				.layer(4, new DenseLayer.Builder()
-						.name("ffn1")
-						.activation(Activation.RELU)
-						.nOut(500)
-						.build())
+			// fully connected
+			.layer(4, new DenseLayer.Builder()
+				.name("ffn1")
+				.activation(Activation.RELU)
+				.nOut(500)
+				.build())
 				
-				// output
-				.layer(5, new OutputLayer.Builder(LossFunctions.LossFunction.MCXENT)
-						.name("output")
-                        .nOut(EpilepticSeizureDetection.numClasses)
-                        .activation(Activation.SOFTMAX) // radial basis function required
-                        .build())
+			// output
+			.layer(5, new OutputLayer.Builder(LossFunctions.LossFunction.MCXENT)
+				.name("output")
+                                .nOut(EpilepticSeizureDetection.numClasses)
+                                .activation(Activation.SOFTMAX) // radial basis function required
+                                .build())
 				
-				.setInputType(InputType.convolutionalFlat(params.getWidth(), params.getHeight(), params.getChannels()))
-				.build();
+			.setInputType(InputType.convolutionalFlat(params.getWidth(), params.getHeight(), params.getChannels()))
+			        .build();
 				
 		return new MultiLayerNetwork(conf);
 	}
